e7543860cd7b925bcf34a61818f095b4f5af6f5c
Nlp Trinh <torrances@yahoo.com>
2015-04-14 23:06:09 -0700

    add solr

---
+++ solr/run-solr.sh
@@ -0,0 +1,16 @@
+SRC=solr_data
+TGT=~/solr_data
+
+if [ ! -d "$TGT" ]; then
+	echo "creating data volume ..."
+	mkdir -p $TGT/conf
+	cp $SRC/core.properties $TGT
+	cp $SRC/conf/* $TGT/conf
+	mkdir -p $TGT/data
+	chmod -R 777 $TGT
+else
+	echo "data volume already exists"
+fi
+
+sudo docker run -p 8983:8983 -v /home/craig/solr_data/:/opt/solr/server/solr/books craig/solr
+#sudo docker run -p 8983:8983 -v /home/craig/workspaces/public/docker/solr/solr_data:/opt/solr/server/solr/books craig/solr
