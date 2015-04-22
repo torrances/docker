75d196ef9a66ca41e8977f52ce7ff5ba02ceccda
Craig Trim <craigtrim@gmail.com>
2015-04-22 10:14:03 -0700

    update run scripts

--- apache/maven/run.sh
+++ apache/maven/run.sh
@@ -1,5 +1,18 @@
-# sudo docker-compose up
-docker \
+################################################################
+##
+## PURPOSE:
+## build a maven container
+## this image isn't meant to be launched as a standalone container
+## but provides an extension base for many other images
+##
+## HISTORY:
+## 10:05 AM 4/22/2015 created 
+##
+## PREREQUSITES:
+## 1. docker > 1.4
+##
+################################################################
+
+sudo docker \
   build --no-cache -t \
   craig/maven .
-
--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,10 +1,23 @@
+################################################################
+##
+## PURPOSE:
+## build and launch a jenkins container
+##
+## HISTORY:
+## 10:05 AM 4/22/2015 created 
+##
+## PREREQUSITES:
+## 1. docker > 1.4
+## 2. build craig/maven <https://github.com/torrances/docker/tree/master/apache/maven>
+##
+################################################################
 mkdir ~/jpr
 
-docker build \
+sudo docker build \
   --no-cache -t \
   craig/jenkins .
 
-docker run \
+sudo docker run \
   -p 8040:8080 \
   -v ~/jpr:/var/jenkins_home \
   craig/jenkins
